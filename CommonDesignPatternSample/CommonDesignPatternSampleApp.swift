cf2ee8bb4ef1efef08a397c39885f81d106f03b8
thanhphong070 <187650-thanhphong070@users.noreply.gitlab.com>
2021-07-10 21:14:54 +0700

    Initial Commit

---
+++ CommonDesignPatternSample/CommonDesignPatternSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommonDesignPatternSampleApp.swift
+//  CommonDesignPatternSample
+//
+//  Created by phong070 on 10/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CommonDesignPatternSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CommonDesignPatternSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CommonDesignPatternSample
+//
+//  Created by phong070 on 10/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
